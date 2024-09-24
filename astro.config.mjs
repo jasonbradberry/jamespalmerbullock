da038250dd80607c79965acf6b444479aecb605b
Jason Bradberry <jason@futureselves.uk>
2024-09-24 19:12:43 +0100

    Update config with custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jasonbradberry.github.io', // Site URL
-    base: '/jamespalmerbullock', // Base URL for GitHub Pages deployment
+    site: 'https://www.jamespalmerbullock.co.uk', // Site URL
     integrations: [
         mdx(),
         sitemap(),
